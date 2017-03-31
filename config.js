d0945ea19f752b7635bcd9f7e6f824ad0ca27471
Cristian Baldi <bld.cris.96@gmail.com>
2017-03-31 21:52:23 +0200

    Add comments to config file

--- config.js
+++ config.js
@@ -1,3 +1,8 @@
+/* Each language object has 3 attributes:
+	+ filename, for specifying which file inside the `i18n` directory should be used
+	+ fallback, another language key that will be used as a fallback, if null the raw translation key will be used
+	+ isRTL, a boolean flag to tell if the language is right-to-left
+*/
 languages = {
 	'it': {
 		filename: 'it',
@@ -11,12 +16,14 @@ languages = {
 	},
 }
 
+// the language that will be used in the building process of the skin
 const currentLanguage = process.env.LANGUAGE || 'en'
 if (!languages[currentLanguage]) {
 	throw("Undefined language: " + currentLanguage)
 }
 const language = languages[currentLanguage]
 
+// the environment that will be considered when building the skin, either `production` or `development`
 const nodeEnv = process.env.NODE_ENV || 'development'
 
 module.exports = {
