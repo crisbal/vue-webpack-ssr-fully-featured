e9e2a4b7f4b119efcaff1a6a643000fa3a885794
Cristian Baldi <bld.cris.96@gmail.com>
2017-04-06 18:53:41 +0200

    Force doublequotes in JS

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,48 +1,48 @@
-const config = require('./config')
+const config = require("./config")
 
 module.exports = {
 	parserOptions: {
-		sourceType: 'module'
+		sourceType: "module"
 	},
-	extends: 'google',
+	extends: "google",
 
 	// required to lint *.vue files
 	plugins: [
-		'html'
+		"html"
 	],
 
 	settings: {
-		'import/resolver': {
-			'webpack': {
-				'config': 'build/webpack.base.conf.js'
+		"import/resolver": {
+			"webpack": {
+				"config": "build/webpack.base.conf.js"
 			}
 		}
 	},
 
 	// add your custom rules here
 	rules: {
-		// don't allow semicolons
-		'semi': ['error', 'never'],
+		// don"t allow semicolons
+		"semi": ["error", "never"],
 
-		// don't require comma in the last line of an object/dictionary declaration
-		'comma-dangle': 0,
+		// don"t require comma in the last line of an object/dictionary declaration
+		"comma-dangle": 0,
 
 		// ignore max-len for comments
-		'max-len': [
-			'error',
-			{ 'ignoreComments': true, 'ignoreTrailingComments': true }
+		"max-len": [
+			"error",
+			{ "ignoreComments": true, "ignoreTrailingComments": true }
 		],
 
 		// force space after and before curly braces in object/dict declarations
-		'object-curly-spacing': ['error', 'always'],
+		"object-curly-spacing": ["error", "always"],
 
 		// allow debugger; instruction during development
-		'no-debugger': config.isProduction ? 2 : 0,
+		"no-debugger": config.isProduction ? 2 : 0,
 
 		// force "===" in comparisons when ambiguous
-		'eqeqeq': ['error', 'smart'],
+		"eqeqeq": ["error", "smart"],
 
 		// force double quotes
-		'quotes': ['error': 'double']
+		"quotes": ["error", "double"]
 	}
 }
