f52b291d7a9811ec649fe69cd149ba7f4ad55d1e
Cristian Baldi <bld.cris.96@gmail.com>
2017-05-12 18:22:06 +0200

    [router] Enable showcase only on development and on testing

--- src/router/index.js
+++ src/router/index.js
@@ -6,17 +6,21 @@ Vue.use(Router)
 // We are also using Webpack code splitting here so that each route's associated
 // component code is loaded on-demand only when the route is visited.
 // When do you use on-demand load? When the view is not one of the important one. When a route is important? You decide
-
 import Home from "views/Home.vue" // include in the main bundle
-const Showcase = () => System.import("views/Showcase.vue") // load dynamically when needed
+
+let routes = [
+	{ path: "/", component: Home }
+]
+
+if (process.env.NODE_ENV !== "production") {
+	const Showcase = () => System.import("views/Showcase.vue") // load dynamically when needed
+	routes.push({ path: "/showcase", component: Showcase })
+}
 
 export function createRouter() {
 	return new Router({
 		mode: "history",
 		scrollBehavior: () => ({ y: 0 }),
-		routes: [
-			{ path: "/", component: Home },
-			{ path: "/showcase", component: Showcase }
-		]
+		routes
 	})
 }
