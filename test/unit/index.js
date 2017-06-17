2b3701c022659ad218f80157cec493ff575250fb
Cristian Baldi <bld.cris.96@gmail.com>
2017-06-17 16:50:32 +0200

    [test] Revert testing to PhantomJS

--- test/unit/index.js
+++ test/unit/index.js
@@ -2,6 +2,9 @@
 /* eslint-disable no-extend-native */
 Function.prototype.bind = require("function-bind")
 
+// Polyfill Promise for PhantomJS
+import "es6-promise/auto"
+
 // require all test files (files that ends with .spec.js)
 const testsContext = require.context("./specs", true, /\.spec$/)
 testsContext.keys().forEach(testsContext)
